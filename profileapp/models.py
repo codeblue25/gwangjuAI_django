a626cfc9b6e46150cecaeba773af024e3b9a10bc
codeblue25 <chois1031@gmail.com>
2021-07-22 15:54:09 +0900

    210722 commit3

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,4 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='profile')        # 1계정당 1프로필 매칭
+
+    image = models.ImageField(upload_to='profile/', null=True) # 꼭 넣을 필요는 없는 이미지
+
+    nickname = models.CharField(max_length=30, unique=True)    # 고유한 닉네임
+
+    message = models.CharField(max_length=200, null=True)      # 꼭 적을 필요는 없는 상태 메시지

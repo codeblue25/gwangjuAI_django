c7480442f7cd513d3b30dd41beefcca056740ab6
codeblue25 <chois1031@gmail.com>
2021-07-28 09:25:23 +0900

    210728 commit1

--- gis_1ban_1/urls.py
+++ gis_1ban_1/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include ('profileapp.urls')),  # profiles 안에있는 profilesapp의 urls으로 하위분기하라
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # 임시 이미지 경로 설정
--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from profileapp.views import ProfileCreateView
+from profileapp.views import ProfileCreateView, ProfileUpdateView
 
 app_name = 'profileapp'
 
 urlpatterns = [
-    path('create/', ProfileCreateView.as_view(), name='create')
+    path('create/', ProfileCreateView.as_view(), name='create'),
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'),  # 무엇을 update할지 특정지어야하기 때문에 primary key를 넣어야함
 ]

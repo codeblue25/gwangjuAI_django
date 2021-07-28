1f5afb02d608d18e19c9e69e20fe9c006895d410
codeblue25 <chois1031@gmail.com>
2021-07-28 10:35:46 +0900

    210728 commit4

--- accountapp/views.py
+++ accountapp/views.py
@@ -35,8 +35,10 @@ def hello_world(request):
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')  # 함수와 달리 클래스는 작동하기 위해 '_lazy'를 붙여야함(?)
     template_name = 'accountapp/create.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 
 class AccountDetailView(DetailView):
@@ -54,8 +56,10 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 
 @method_decorator(has_ownership, 'get')

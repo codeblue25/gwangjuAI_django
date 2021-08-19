42e4aade5b961883e9cc6f81bd0865f7d25ced38
codeblue25 <chois1031@gmail.com>
2021-08-19 15:01:52 +0900

    210819 commit2

--- gis_1ban_1/urls.py
+++ gis_1ban_1/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('articles/', include ('articleapp.urls')),
     path('comments/', include('commentapp.urls')),
     path('projects/', include('projectapp.urls')),
-    path('subscribes/', include('subscribeapp.urls'))
+    path('subscribe/', include('subscribeapp.urls'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # 임시 이미지 경로 설정

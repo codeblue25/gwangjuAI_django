f1ed9c2c36d22d3712e360de319ce4e7ffa0eba8
codeblue25 <chois1031@gmail.com>
2021-08-23 15:58:18 +0900

    210823 commit4

--- likeapp/models.py
+++ likeapp/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from articleapp.models import Article
+
+
+class LikeRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='like_record', null=False)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,
+                                related_name='like_record', null=False)
+
+    class Meta:
+        unique_together = ['user', 'article']   # 한 게시물에는 한 개의 좋아요만 할 수 있도록

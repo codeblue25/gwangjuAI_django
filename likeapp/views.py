7b2e6efa11cc053f06e25e0edda6b971c89ce0af
codeblue25 <chois1031@gmail.com>
2021-08-25 09:42:31 +0900

    210825 commit2
    좋아요 반영 여부 확인

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -22,13 +23,16 @@ class LikeArticleView(RedirectView):
                                                article=article)
 
         if likeRecord.exists():      # 좋아요 기록이 존재하면, 해당 게시물로 돌아감
+            # 좋아요가 반영되지 않음
+            messages.add_message(request, messages.ERROR, '좋아요는 한번만 가능합니다.')
             return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk':kwargs['article_pk']}))
         else:                        # 좋아요 기록이 아직 없을 때
             LikeRecord(user=user, article=article).save()
 
         article.like += 1
         article.save()
-
+        # 좋아요가 반영됨
+        messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다.')
         return super().get(request, *args, **kwargs)
 
     def get_redirect_url(self, *args, **kwargs):
